5b8b2eabffcf9ea23c525ad83e9baa379e7e4c5b
scornelius <scornelius@gmail.com>
2011-06-06 12:12:33 +0100

    poisson calcs working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,17 @@ class PagesController < ApplicationController
   	@rr = relativeRisk(123,115.8)
   	@lcl = byarsLCL(123,115.8)
   	@ucl = byarsUCL(123,115.8)
+  	@templ = poissonLCL(178.35,0.001)
+  	@temph = poissonUCL(178.35,0.001)
+  	
+  	@one = 1.45.ceil
+  	@two = 1.98.ceil
+  	
+#  =IF(ISBLANK(L5),"",(((K5-ROUNDDOWN(K5,0))*(poishigh(ROUNDUP(K5,0),0.001)/ROUNDUP(K5,0)-poishigh(ROUNDDOWN(K5,0),0.001)/ROUNDDOWN(K5,0)))+poishigh(ROUNDDOWN(K5,0),0.001)/ROUNDDOWN(K5,0))*100)
+# Ulm K. A simple method to calculate the confidence interval of a standardized mortality ratio. American Journal of Epidemiology 1990;131(2):373-375.
+# Dobson AJ, Kuulasmaa K, Eberle E, Scherer J. Confidence intervals for weighted sums of Poisson parameters. Statistics in Medicine 1991;10:457-462.
+# http://onlinelibrary.wiley.com/doi/10.1002/sim.4780100317/abstract
+
   	
   end
 
--- lib/statsfunctions.rb
+++ lib/statsfunctions.rb
@@ -37,6 +37,114 @@ module Statsfunctions
     
     return ucl
   end
+  
+  #functions to calculate 99.8% poisson control limits
+  
+  def poissonLCL(expected,tolerance)
+    
+    if expected < 5
+      lcl = 0
+    else
+      lcl = ((expected-expected.truncate) * (poissonLow(expected.ceil,0.001)/expected.ceil - poissonLow(expected.truncate,0.001)/expected.truncate) + poissonLow(expected.truncate,0.001)/expected.truncate) * 100     
+    end
+    
+    return lcl.round(1)
+    
+  end
+  
+  def poissonUCL(expected, tolerance)
+    
+    ucl = ((expected-expected.truncate) * (poissonHigh(expected.ceil,0.001)/expected.ceil - poissonHigh(expected.truncate,0.001)/expected.truncate) + poissonHigh(expected.truncate,0.001)/expected.truncate) * 100  
+          
+    return ucl.round(1)
+          
+  end
+  
+  
+  def poissonLow(vz,pl)
+    if vz <= 0 or pl < 0 or pl > 1 or vz != vz.truncate
+      poisLow = 0
+    else
+      v = 0.5
+      dv = 0.5
+      
+      while dv > 1e-7
+        dv = dv/2
+        if poissonP((1+vz)*v/(1-v),vz,1e10) > pl
+          v = v-dv
+        else
+          v = v+dv
+        end
+      end
+      
+      poisLow = (1+vz) * v/(1-v)    
+    end
+    
+    return poisLow.round(1) 
+  end
+  
+  def poissonHigh(vz,ph)
+    if vz <= 0 or ph < 0 or ph > 1 or vz != vz.truncate
+      poisHigh = 0
+    else
+      v = 0.5
+      dv = 0.5
+      
+      while dv > 1e-7
+        dv = dv/2
+        if poissonP((1+vz)*v/(1-v),0,vz) < ph
+          v = v-dv
+        else
+          v = v+dv
+        end
+      end
+      
+      poisHigh = (1+vz) * v/(1-v)    
+    end
+    
+    return poisHigh.round(1) 
+  end  
+  
+  
+  def poissonP(z,x1,x2)
+    if z < 0
+      poisP = 0
+    elsif x1 < 0
+      poisP = 0
+    elsif x2 < 0
+      poisP = 0
+    elsif x2 < x1
+      poisP = 0
+    elsif x1 != x1.truncate
+      poisP = 0
+    elsif x2 != x2.truncate
+      poisP = 0
+    else
+      q = 1
+      total = 0
+      s = 0
+      k = 0
+      
+      while k <= z or q > (total * 1e-10)
+        total = total + q
+        if k >= x1 and k <=x2
+          s = s + q
+        end
+        if total > 1e30
+          s = s/1e30
+          total = total/1e30
+          q = q/1e30
+        end
+        k = k + 1
+        q = q * z/k
+      end
+      
+      poisP = s/total
+    end
+    
+    return poisP
+        
+  end
       
   
   
